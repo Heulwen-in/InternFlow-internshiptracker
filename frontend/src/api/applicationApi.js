67bae2d51b4d21de36808ed6ddfe400e32909d5d
Heulwen-in <dungprostin@gmail.com>
2026-05-20 19:29:41 +0700

    frontend API helpers

---
+++ frontend/src/api/applicationApi.js
@@ -0,0 +1,7 @@
+import api from "./axios";
+
+export const getApplications = () => api.get("/applications");
+export const getApplication = (id) => api.get(`/applications/${id}`);
+export const createApplication = (data) => api.post("/applications", data);
+export const updateApplication = (id, data) => api.put(`/applications/${id}`, data);
+export const deleteApplication = (id) => api.delete(`/applications/${id}`);
