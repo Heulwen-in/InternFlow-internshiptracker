d56d9264dafd6db3ed45214a8ec34cd782599ff3
Heulwen-in <dungprostin@gmail.com>
2026-05-29 20:55:12 +0700

    Create task routes

---
+++ backend/src/routes/taskRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const protect = require("../middleware/authMiddleware");
+const {
+  getTasks,
+  createTask,
+  updateTask,
+  deleteTask,
+} = require("../controllers/taskController");
+
+const router = express.Router();
+
+router.use(protect);
+
+router.get("/tasks", getTasks);
+router.post("/tasks", createTask);
+router.put("/tasks/:id", updateTask);
+router.delete("/tasks/:id", deleteTask);
+
+module.exports = router;
