f4ca1161a1bf0728f49942fb20e379310eed1dee
Heulwen-in <dungprostin@gmail.com>
2026-06-19 23:25:22 +0700

    Update database

---
+++ backend/prisma/migrations/20260616120000_add_user_profile/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "avatarUrl" TEXT,
+ADD COLUMN     "avatarHue" INTEGER,
+ADD COLUMN     "bio" TEXT,
+ADD COLUMN     "school" TEXT,
+ADD COLUMN     "graduationYear" INTEGER,
+ADD COLUMN     "targetRole" TEXT;
