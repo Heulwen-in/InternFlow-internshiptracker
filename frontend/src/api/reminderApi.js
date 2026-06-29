8bf1922bb7280b7f65dad7c918bbeff5354ba41a
Heulwen-in <dungprostin@gmail.com>
2026-06-29 21:10:41 +0700

    add Reminder api

---
+++ frontend/src/api/reminderApi.js
@@ -0,0 +1,6 @@
+import api from "./axios";
+
+export const getNotifications = () => api.get("/notifications");
+export const markNotificationRead = (id) => api.patch(`/notifications/${id}/read`);
+export const markAllNotificationsRead = () => api.patch("/notifications/read-all");
+export const sendDailyDigest = () => api.post("/reminders/digest");
